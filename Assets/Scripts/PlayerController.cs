f7183380468e771d5232e0f6809db0c48fd1199e
Josh-VGAM <jtr.eck24@gmail.com>
2026-06-22 20:33:10 -0500

    character look input

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,19 +1,26 @@
-using Unity.VisualScripting;
+//using System.Numerics;
+using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
     [Header("Movement")]
     public float speed = 0f;
-    private Vector2 move;
+    private Vector2 move, mouseLook;
+    private Vector3 rotationTarget;
 
     private Rigidbody rb;
+    public bool isPc = true;
 
     public void OnMove(InputAction.CallbackContext context)
     {
         move = context.ReadValue<Vector2>();
+    }
+
+    public void OnMouseLook(InputAction.CallbackContext context)
+    {
+        mouseLook = context.ReadValue<Vector2>();
     }
 
     void Start()
@@ -26,17 +33,38 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         MovePlayer();
+
+        if (isPc)
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(mouseLook);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                rotationTarget = hit.point;
+            }
+            playerLook();
+        }
     }
 
     public void MovePlayer()
     {
         Vector3 movement = new Vector3(move.x, 0f, move.y);
 
-        if(movement != Vector3.zero)
-        {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
-        }
-
         transform.Translate(movement * speed * Time.deltaTime, Space.World);
+    }
+
+    public void playerLook()
+    {
+        var lookPos = rotationTarget - transform.position;
+        lookPos.y = 0;
+        var rotation = Quaternion.LookRotation(lookPos);
+
+        Vector3 aimDirection = new Vector3(rotationTarget.x, 0f, rotationTarget.y);
+
+        if (aimDirection != Vector3.zero)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 0.15f);
+        }
     }
 }
