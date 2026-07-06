0c49947c13ff3776065548717879c4140cabd490
Josh-VGAM <jtr.eck24@gmail.com>
2026-07-05 19:10:56 -0500

    Ai fix Now workin on raycast

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,3 +1,4 @@
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -6,27 +7,31 @@ public class Enemy : MonoBehaviour
     [Header("AI")]
     private PlayerController player;
     private NavMeshAgent enemyAI;
-    public float detectionRadius = 2f;
-    private Collider[] hitColliders;
+    public GameObject meleeRadius;
+    private float detection;
+
+    //Temporary delete
+    /*private Collider[] hitColliders;
     private const int maxColliders = 1;
-    public LayerMask playerLayer;
+    public LayerMask playerLayer;*/
 
     [Header("Animations")]
-    public Animator meleeAnim;
+    //public Animator meleeAnim;
     //private string meleeAnimation = "MeleeAnimation";
     public bool isAttacking = false;
 
     void Awake()
     {
-        hitColliders = new Collider[maxColliders];
+        //hitColliders = new Collider[maxColliders];
+        player = FindAnyObjectByType<PlayerController>(); 
         enemyAI = GetComponent<NavMeshAgent>();
     }
 
     void Start()
     {
-        meleeAnim = GetComponent<Animator>();
+        //meleeAnim = GetComponent<Animator>();
     }
-    
+
     void Update()
     {
         //PlayerHitDetect();
@@ -35,7 +40,7 @@ public class Enemy : MonoBehaviour
 
     void PlayerHitDetect()
     {
-        int detectionHit = Physics.OverlapSphereNonAlloc(transform.position, detectionRadius, hitColliders, playerLayer);
+        //int detectionHit = Physics.OverlapSphereNonAlloc(transform.position, detectionRadius, hitColliders, playerLayer);
 
         //for (int i = 0; i < detectionHit; i++)
             /*if (hitColliders[i].TryGetComponent<PlayerController>(out PlayerController player))//redfine no need for hitcolliders
@@ -47,9 +52,13 @@ public class Enemy : MonoBehaviour
 
     void PlayerDetect()
     {
-        if (player != null)
+        //Vector3 directionToPlayer = (player.transform.position - transform.position).normalized;//get direction
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
+        if (Physics.Raycast(transform.position, forward, out RaycastHit hit, detection))
         {
+            Debug.Log("Player detected");
             enemyAI.SetDestination(player.transform.position);
         }
+        Debug.DrawRay(transform.position, forward, Color.red);
     }
 }
