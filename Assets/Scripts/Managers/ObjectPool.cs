9d185e30805dad42b02b7e156ecb43a4fa59316f
Josh-VGAM <jtr.eck24@gmail.com>
2026-07-18 20:07:09 -0500

    Organized folders in scripts

---
+++ Assets/Scripts/Managers/ObjectPool.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{   
+    //[SerializeField] ObjectPool<GameObject> objectPool;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
